49942f1811bb48e729e745ce1313d4116d2aebdd
Luke Lim <z5210599@unsw.edu.au>
2022-06-08 16:41:31 +1000

    added channelMessagesV1

--- src/channel.js
+++ src/channel.js
@@ -1,3 +1,4 @@
-function channelInviteV1 (authUserId, channelId, uId) {
-    return 'authUserId' + 'channelId' + 'uId';
+function channelMessagesV1 (authUserId, channelId, start) {
+    return 'authUserId' + 'channelId' + 'start';
 }
+
