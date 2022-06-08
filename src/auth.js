ee18b7d51028852640708b8f12eda538abf029ce
Rick <z5191423@ad.unsw.edu.au>
2022-06-08 15:39:12 +1000

    Rick's first try of adding auth.js

--- src/auth.js
+++ src/auth.js
@@ -3,3 +3,9 @@ function authLoginV1 (email, password) {
     return 'email' + 'password';
 
 }
+
+function authRegisterV1 ( email, password, nameFirst, nameLast ) {
+    
+    return 'email' + 'password' + 'nameFirst' + 'nameLast';
+
+}
