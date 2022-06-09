72533b234ec5a2f6d3636f16f268b11e98a36fea
Elizabeth Law <elizabeth.law@student.unsw.edu.au>
2022-06-09 20:21:35 +1000

    added 2 new stub functions

--- src/channel.js
+++ src/channel.js
@@ -2,6 +2,14 @@ function channelInviteV1 (authUserId, channelId, uId) {
     return 'authUserId' + 'channelId' + 'uId';
 }
 
-function channelMessagesV1 (authUserId, channelId, start) {
+function channelMessagesV1 (authUserId, channelId) {
     return 'authUserId' + 'channelId' + 'start';
+}
+
+function channelDetailsV1 (authUserId, channelId) {
+    return 'authUserId' + 'channelId';
+}
+
+function channelJoinV1 (authUserId, channelId, start) {
+    return 'authUserId' + 'channelId';
 }
