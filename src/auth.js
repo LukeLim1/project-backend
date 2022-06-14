049d57d2043fec7aa9013ecac430bfdc89780d70
z5312386 <z5312386@cse.unsw.edu.au>
2022-06-14 20:49:51 +1000

    First attempt at writing test code for auth and channels

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,14 @@
 function authLoginV1 (email, password) {
     
-    return 'email' + 'password';
+    
+    return email + password;
 
 }
 
 function authRegisterV1 ( email, password, nameFirst, nameLast ) {
     
-    return 'email' + 'password' + 'nameFirst' + 'nameLast';
+    return email + password + nameFirst + nameLast;
 
 }
+
+export { authLoginV1, authRegisterV1 };
