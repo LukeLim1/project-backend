60e0341778aa3fdb48c86e6d4e775a800ed84003
Luke Lim <z5210599@unsw.edu.au>
2022-06-21 11:31:16 +1000

    included more test cases for channel functions

--- src/channel.test.js
+++ src/channel.test.js
@@ -40,14 +40,42 @@ test('Testing successful channelDetailsV1', () => {
     ]);
 });
 
-// code for testing channelJoinV1...
-test('Testing successful channelJoinV1', () => {
+// Error cases for channelJoinV1
+
+test('channelId does not refer to valid channel', () => {
     clearV1();
-    
-    channelJoinV1(4, 121212);
+    const owner = authRegisterV1('owner@email.com', '123456', 'Ada', 'Bob');
+    const user1 = authRegisterV1('user1@email.com', '987654', 'Ocean', 'Hall');
+    const channel1 = channelsCreateV1(owner.authUserId, 'channel#1', true);
+    expect(channelJoinV1(user1.authUserId, channel1.channelId + 5)).toMatchObject({error: 'error'});
+
+    // Same test for channelDetailsV1
+    expect(channelDetailsV1(owner.authUserId, channel1.channelId + 5)).toMatchObject({error: 'error'});
 });
 
-// code for testing error cases for channelDetailsV1...
+test('authorised user is already a member', () => {
+    clearV1();
+    const owner = authRegisterV1('owner@email.com', '123456', 'Ada', 'Bob');
+    const user1 = authRegisterV1('user1@email.com', '987654', 'Ocean', 'Hall');
+    const channel1 = channelsCreateV1(owner.authUserId, 'channel#1', true);
+    channelJoinV1(user1.authUserId, channel1.channelId);
+    expect(channelJoinV1(user1.authUserId, channel1.channelId)).toMatchObject({error: 'error'});
+});
 
+test('channelId refers to private channel and the user is not channel member nor global owner', () => {
+    clearV1();
+    const owner = authRegisterV1('owner@email.com', '123456', 'Ada', 'Bob');
+    const user1 = authRegisterV1('user1@email.com', '987654', 'Ocean', 'Hall');
+    const channel1 = channelsCreateV1(owner.authUserId, 'channel#1', false);
+    expect(channelJoinV1(user1.authUserId, channel1.channelId)).toMatchObject({error: 'error'});
+});
 
-// code for testing error cases for channelJoinV1...
+// Error cases for channelDetailsV1
+
+test('channelId valid, but the user is not a member', () => {
+    clearV1();
+    const owner = authRegisterV1('owner@email.com', '123456', 'Ada', 'Bob');
+    const user1 = authRegisterV1('user1@email.com', '987654', 'Ocean', 'Hall');
+    const channel1 = channelsCreateV1(owner.authUserId, 'channel#1', false);
+    expect(channelDetailsV1(user1.authUserId, channel1.channelId)).toMatchObject({error: 'error'});
+});
