c7bdde88601db41f4531077d60b0c75261ebca07
z5312386 <z5312386@cse.unsw.edu.au>
2022-06-24 14:00:39 +1000

    trying to restore the master branch

--- src/channels.js
+++ src/channels.js
@@ -1,21 +1,10 @@
 import { getData, setData} from './dataStore';
-function channelsListV1 (authUserId) {
-    
-    return authUserId;
-
-}
-
-function channelsListallV1 (authUserId) {
-
-    return authUserId;
-
-}
 
 function channelsCreateV1 ( authUserId, name, isPublic ) {
     const data = getData();
-    let randomNumber = 1;
+    let randomNumber = Math.floor(Math.random() * 1000);
     if (data.usedNums.length !== 0) {
-        randomNumber += data.usedNums[data.usedNums.length - 1]
+        randomNumber += data.usedNums[data.usedNums.length - 1];
     }
     data.usedNums.push(randomNumber);
 
@@ -26,7 +15,7 @@ function channelsCreateV1 ( authUserId, name, isPublic ) {
     
     data.channels.push({
         name: `${name}`,
-        isPublic: `${isPublic}`, 
+        isPublic: isPublic, 
         ownerMembers: [authUserId],
         allMembers: [authUserId],
         channelId: randomNumber,
@@ -34,5 +23,54 @@ function channelsCreateV1 ( authUserId, name, isPublic ) {
     setData(data);
     return {channelId: randomNumber};
 }
+
+
+
+function channelsListV1 (authUserId) {
+    
+    const data = getData();
+
+    const obj_arr = [];
+
+    for (const channel of data.channels) {
+        if (channel.allMembers.includes(authUserId)) {
+            const channels_obj = {
+                channelId: channel.channelId,
+                name: channel.name,
+            }
+        
+            obj_arr.push(channels_obj);
+        
+        }
+
+    }
+
+    return obj_arr;
+
+}
+
+function channelsListallV1 (authUserId) {
+
+    const data = getData();
+
+    const obj_arr = [];
+
+    for (const element of data.channels) {
+
+        const channels_obj = {
+            channelId: element.channelId,
+            name: element.name,
+        }
+        
+        obj_arr.push(channels_obj);
+    
+    }
+
+
+    return obj_arr;
+
+}
+
+
 
 export { channelsListV1, channelsListallV1, channelsCreateV1 };
