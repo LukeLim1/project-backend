e0adbcbfdb57205aeae4b8efdf5f17034c7af4df
Luke Lim <z5210599@unsw.edu.au>
2022-07-06 18:53:34 +1000

    changed dataStore.js to dataStore.ts

---
+++ src/dataStore.ts
@@ -0,0 +1,43 @@
+// YOU SHOULD MODIFY THIS OBJECT BELOW
+
+import { userTemplate } from './channel';
+
+interface dataTemplate {
+  users: userTemplate[];
+  channels: any[];
+  usedNums: number[];
+}
+
+let data: dataTemplate = {
+  users: [],
+  channels: [],
+  usedNums: [],
+};
+
+// YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
+
+/*
+Example usage
+    let store = getData()
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Rando'] }
+
+    names = store.names
+
+    names.pop()
+    names.push('Jake')
+
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Jake'] }
+    setData(store)
+*/
+
+// Use get() to access the data
+function getData() {
+  return data;
+}
+
+// Use set(newData) to pass in the entire data object, with modifications made
+function setData(newData: dataTemplate) {
+  data = newData;
+}
+
+export { getData, setData };
